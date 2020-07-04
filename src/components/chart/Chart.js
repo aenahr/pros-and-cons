e76fa2ed90f4a4c301c9f8c47f48b4e8aad01204
Aenah Ramones <aenah.ramones@gmail.com>
2020-07-03 18:23:51 -0700

    add chart title with bold and italicize capabilities

--- src/components/chart/Chart.js
+++ src/components/chart/Chart.js
@@ -1,14 +1,55 @@
 import React from 'react';
 import "./Chart.css";
-import { FormControl, FormGroup, ControlLabel, HelpBlock, Checkbox, Radio, Button } from 'react-bootstrap';
+import { FormControl, Button, Form, InputGroup } from 'react-bootstrap';
 
 export default class Chart extends React.Component {
+
+    constructor(){
+        super();
+        this.state = {
+            title: "",
+            bold: false,
+            italic: false,
+        }
+
+        this.toggleBold = this.toggleBold.bind(this);
+        this.toggleItalic = this.toggleItalic.bind(this);
+
+    }
+
     
     render() {
         return(
             <div id="chart">
-                <p>Hello world</p>
+                <h1 id="chartTitle" className={(this.state.bold ? 'bold' : '') + " " + (this.state.italic ? 'italic' : '')}>{this.state.title}</h1>
+                <Form>
+                    <InputGroup>
+                        <InputGroup.Append className="inputLabel">
+                            <InputGroup.Text>Chart Title</InputGroup.Text>
+                        </InputGroup.Append>
+                        <FormControl required type="text" placeholder="enter title" className="input" value={this.state.value} onChange={this.handleTitleChange} />
+                        <Form.Control.Feedback type="invalid">Please add a title for your chart.</Form.Control.Feedback>
+                        <Form.Check className="checkbox" checked={this.state.bold} onChange={this.toggleBold} label={"bold"} />
+                        <Form.Check className="checkbox" checked={this.state.italic} onChange={this.toggleItalic} label={"italicize"} />
+                    </InputGroup>
+                    <Button className="button tertiary">Create Table</Button>
+                </Form>
             </div>
         )
+    }
+
+    /** Function that will bold or un-bold the title */
+    toggleBold(){
+        this.setState({ bold: !this.state.bold});
+    }
+
+    /** Function that will italicize or un-italicize the title */
+    toggleItalic(){
+        this.setState({ italic: !this.state.italic});
+    }
+
+    /** Function that changes the value of of the chart title as the user types */
+    handleTitleChange = (e) => {
+        this.setState({title: e.target.value});
     }
 }
